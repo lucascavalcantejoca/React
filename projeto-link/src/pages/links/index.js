7321f046e197e5909e73e99a29423a2def6c7233
Lucas Cavalcante Joca Eugênio <lucasjoca27@gmail.com>
2025-06-10 16:29:57 -0300

    att de estilos

--- projeto-link/src/pages/links/index.js
+++ projeto-link/src/pages/links/index.js
@@ -1,13 +1,21 @@
 import './links.css';
-import {FiArrowLeft, FiTrash} from 'react-icons/fi';
+import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi';
 
 export default function Links() {
   return(
     <div className="links-container">
 
       <div className='links-header'>
-        <FiArrowLeft/>
+        <FiArrowLeft size={38} color='white'/>
         <h1>Meus Links</h1>
+      </div>
+
+      <div className='links-item'>
+        <FiLink size={18} color='white'/>
+        <button>https://www.youtube.com/</button>
+        <button>
+          <FiTrash size={24} color='#FF5454'/>
+        </button>
       </div>
 
     </div>
