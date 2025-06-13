563d3aba168b132c44cad9bb020f071a5255e774
Lucas Cavalcante Joca Eugênio <lucasjoca27@gmail.com>
2025-06-13 16:17:55 -0300

    adição da pagina erro 404

--- projeto-link/src/routes.js
+++ projeto-link/src/routes.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Links from './pages/links';
+import Error from './pages/Error';
 
 export default function RoutesApp() {
     return (
@@ -9,6 +10,7 @@ export default function RoutesApp() {
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/links' element={<Links/>}/>
+                <Route path='*' element={<Error/>}/>
             </Routes>
         </BrowserRouter>
     )
